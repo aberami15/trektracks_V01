ede3c8ed251461d4aa2cdbf2cf8af0eed9d168dc
MrinaliXavier <xmrinali@gmail.com>
2025-03-23 21:19:40 +0530

    expo deployment

--- config.js
+++ config.js
@@ -1,7 +1,7 @@
 const Config = {
 
-    //BASE_URL: 'https://trektracks-be.vercel.app/api',
-    BASE_URL: 'http://192.168.1.6:5000/api',
+    BASE_URL: 'https://trektracks-be.vercel.app/api',
+    //BASE_URL: 'http://192.168.1.6:5000/api',
 
   };
 
